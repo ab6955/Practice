337b788273ad8a22f167597bea0d6fae04a46d56
Abdullah Ansari <143590602+ab6955@users.noreply.github.com>
2024-07-04 10:57:22 +0000

    Variables and data types Done

---
+++ Javascript/Basics.js
@@ -0,0 +1,40 @@
+const name = "Abdullah";
+let rollno = 45;
+let emailId = "ansariabd62@gmail.com";
+let state = "mumbai";
+let city;
+
+// console.log(name);
+// console.log(rollno);
+// console.log(emailId);
+// console.log(state);
+// console.log(city);
+
+// console.log(name, rollno, emailId, state, city);
+// console.table([name, rollno, emailId, state, city]);
+
+const firstName = "Ansari";
+let div = 2;
+let collegeId = "vu8d22";
+let isStudent = true;
+
+// console.log(typeof firstName);
+// console.log(typeof div);
+// console.log(typeof collegeId);
+// console.log(typeof isStudent);
+
+("use strict");
+
+// console.log(typeof null);
+
+let score = "33abc";
+// console.log(score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+let isLogedIn=undefined
+let booleanNumber=Boolean(isLogedIn)
+console.log(booleanNumber);
