441785862b2af0974cd8885066f8744fa40e93a8
Abdullah Ansari <143590602+ab6955@users.noreply.github.com>
2024-07-07 10:57:07 +0000

    Operator precedence operations

--- Javascript/Basics.js
+++ Javascript/Basics.js
@@ -35,6 +35,22 @@ let valueInNumber = Number(score);
 // console.log(valueInNumber);
 // console.log(typeof valueInNumber);
 
-let isLogedIn=undefined
-let booleanNumber=Boolean(isLogedIn)
-console.log(booleanNumber);
+let isLogedIn = undefined;
+let booleanNumber = Boolean(isLogedIn);
+// console.log(booleanNumber);
+
+// console.log(3 ** 2);
+
+let str1 = "Abdullah";
+let str2 = " Ansari";
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+let x = 2;
+let y = x++;
+console.log(`Y:${y} and X:${x}`);
+
+let a = 4;
+let b = ++a;
+console.log(`b:${b} and a:${a}`);
