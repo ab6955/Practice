d2f0559bbff0b17f0afb3ec704c2285faee77c45
Abdullah Ansari <143590602+ab6955@users.noreply.github.com>
2025-01-05 15:10:03 +0000

    Context API part 1

---
+++ React/LAST_REVISION/08miniConetext/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import UserContextProvider from "./context/UserContextProvider";
+import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>React with Chai</h1>
+      <Login />
+      <Profile/>
+    </UserContextProvider>
+  );
+}
+
+export default App;
